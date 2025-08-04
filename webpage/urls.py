7ab1e0685967fc4e2615d972a4ae34b86c9261ad
Pattanapong Lampa <pattanapong.pup@gmail.com>
2025-08-04 12:21:15 +0700

    add multiply

--- webpage/urls.py
+++ webpage/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
-    
-    path('', views.home, name='home'),
+
+    path('', views.home, name='home'), 
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
+    path('multiply/', views.multiply, name='multiply_page'),
 ]
